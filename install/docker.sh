3c78f979c8ce892e7de04abba47ffaa0bb3a2753
EdwinJHLee <edwin.jh.lee@gmail.com>
2018-08-22 15:34:16 +0800

    add docker

--- install/docker.sh
+++ install/docker.sh
@@ -2,6 +2,5 @@ if [ Darwin == $(uname) ]; then
     # source /dev/stdin <<< "$(curl --insecure https://lteam18.github.io/auto/install/docker.mac.sh)";
     curl https://lteam18.github.io/auto/install/docker.mac.sh | bash
 else
-    curl --insecure https://lteam18.github.io/auto/install/docker.mac.sh
-    source /dev/stdin <<< "$(curl --insecure https://lteam18.github.io/auto/install/docker.linux.sh)";
+    curl https://lteam18.github.io/auto/install/docker.linux.sh | bash
 fi
