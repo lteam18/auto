963d913dfbe35dd8754fa5e6465aaa3314d96ab8
EdwinJHLee <edwin.jh.lee@gmail.com>
2018-08-22 15:24:54 +0800

    Add docker install

---
+++ install/docker.linux.sh
@@ -0,0 +1,13 @@
+# step 1: 安装必要的一些系统工具
+apt-get update
+apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+# step 2: 安装GPG证书
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
+# Step 3: 写入软件源信息
+add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+# Step 4: 更新并安装 Docker-CE
+apt-get -y update
+apt-get -y install docker-ce
+
+curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
