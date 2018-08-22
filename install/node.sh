c5555257ef867280fc798b69552edfe08fa19585
EdwinJHLee <edwin.jh.lee@gmail.com>
2018-08-22 15:47:22 +0800

    ready

--- install/node.sh
+++ install/node.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-exec -l $SHELL
+#exec -l $SHELL
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install v8
