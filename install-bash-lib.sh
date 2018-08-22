f373079ebe54cf51fd19d21ad53e92c12ebef8a8
WentingRuan <ethel.ruan@gmail.com>
2018-08-22 20:00:14 +0800

    Update install-bash-lib.sh

--- install-bash-lib.sh
+++ install-bash-lib.sh
@@ -1,8 +1,9 @@
-#! /usr/env/bin bash
+#! /usr/bin/env bash
 
 LT_AUTO_REPO="https://lteam18.github.io/auto/bash-lib"
 
 CUR_DIR=$HOME/.lteam18.auto.bash-lib
+mkdir -p $CUR_DIR
 curl "$LT_AUTO_REPO/manager.sh" > $CUR_DIR/manager.sh
 
 source $CUR_DIR/manager.sh
