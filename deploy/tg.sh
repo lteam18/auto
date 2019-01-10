5ec2878affcce14809c46940b80c9cf157b20a25
Edwin.JH.Lee <edwinjhlee@users.noreply.github.com>
2019-01-10 15:12:14 +0800

    Update tg.sh

--- deploy/tg.sh
+++ deploy/tg.sh
@@ -10,5 +10,8 @@ SERVER="${2:-$(curl http://ipecho.net/plain)}"
 URL="tg://proxy?server=$SERVER&port=$PORT&secret=$RAND_ID"
 echo $URL
 
-curl "https://api.telegram.org/bot767586102:AAEadFW8WMPSwcRoqcSUWLQC0aQ4PUd9ppI/sendMessage?chat_id=-1001336023411&text=$URL"
+MSG="MProto Proxy Effect @ "$(date --date="tomorrow" +"%Y-%m-%d")
 
+curl -i -X POST -H "Content-Type: application/json" -d "{ \"chat_id\": -1001336023411, \"text\": \"[$MSG]($URL)\", \"parse_mode\": \"Markdown\" }" "https://api.telegram.org/bot767586102:AAEadFW8WMPSwcRoqcSUWLQC0aQ4PUd9ppI/sendMessage"
+
+nohup echo $(sleep 2d; docker rm -f telegram-proxy-$PORT) &
