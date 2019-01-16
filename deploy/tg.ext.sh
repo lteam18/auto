9727fc24d4f89d9f2d8d15e25025f370da62d023
EdwinJHLee <edwin.jh.lee@gmail.com>
2019-01-16 16:19:57 +0800

    change deploy

--- deploy/tg.ext.sh
+++ deploy/tg.ext.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
-PORT=${1:?"Please provide port in first argument"}
-GENERATE_ID=$(openssl rand -hex 16)
-RAND_ID=${2:-GENERATE_ID}
+R=$RANDOM"0000"; RANDOM_PORT=33${R:0:3}
+PORT=${1:-$RANDOM_PORT}
+RAND_ID=$(openssl rand -hex 16)
+echo $RAND_ID
 
 NAME="MPROTO-$PORT"
 docker run -d --name $NAME --restart=always -p$PORT:443 -e SECRET=${RAND_ID} telegrammessenger/proxy:latest
@@ -11,6 +12,10 @@ SERVER="${3:-$(curl http://ipecho.net/plain)}"
 URL="tg://proxy?server=$SERVER&port=$PORT&secret=$RAND_ID"
 echo $URL
 
-MSG="Random: mproto://$SERVER:$PORT"
+#DAYS="${2:-365d}"
+
+MSG="mproto://$SERVER:$PORT Expired @ "$(date +"%Y-%m-%d")
 
 curl -i -X POST -H "Content-Type: application/json" -d "{ \"chat_id\": -1001336023411, \"text\": \"[$MSG]($URL)\", \"parse_mode\": \"Markdown\" }" "https://api.telegram.org/bot767586102:AAEadFW8WMPSwcRoqcSUWLQC0aQ4PUd9ppI/sendMessage"
+
+nohup echo $(sleep 2d; docker rm -f $NAME) &
